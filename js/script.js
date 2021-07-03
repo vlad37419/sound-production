a7b1fa76f007085ebbd92bfd34a642f151c2461f
vlad37419 <85108281+vlad37419@users.noreply.github.com>
2021-07-03 20:42:12 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,12 @@
+"use strict"
+
+//Меню бургер
+const iconMenu = document.querySelector('.menu__icon');
+if (iconMenu) {
+  const menuBody = document.querySelector('.menu__body');
+  iconMenu.addEventListener("click", function(e) {
+    document.body.classList.toggle('_lock');
+    iconMenu.classList.toggle('_active');
+    menuBody.classList.toggle('_active');
+  });
+}
